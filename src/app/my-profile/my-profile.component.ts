76dbab13dd1f6f7b0e66921db22375bdf63b700c
Rayane Cristina <68652767+raydevcp20@users.noreply.github.com>
2021-11-06 17:42:49 -0300

    criação da tela de meu perfil

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
+  addLink:boolean = false;
+  newLink: any = '';
+  editingData:boolean = false;
+  arrayLinks: any = [];
+  profile:any = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addNewLink():void{
+    console.log(this.newLink);
+    this.arrayLinks.push(this.newLink);
+    this.addLink = !this.addLink;
+  }
+
+  editData():void{
+    console.log(this.newLink);
+  }
+
+  editProfile():void{
+    console.log(this.newLink);
+  }
 }
