ff729ffeb69464c62ac7de4e3332fb068c16e10a
lauraguarana <lauraguarana@gmail.com>
2021-11-22 21:55:55 -0300

    login page done

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  signUpButton: any = "";
+  signInButton: any = "";
+  container: any = "";
   constructor() { }
 
   ngOnInit(): void {
+  
+    this.container = document.getElementById('container');
+    console.log(this.container);
   }
+
+  signIn(){
+    this.container.classList.remove("right-panel-active");
+  }
+
+  signUp(){
+    this.container.classList.add("right-panel-active");
+  }
+
 
 }
