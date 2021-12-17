b8d82ecb8caa51b1b9de814f0ef1a06e9c9e62d2
Rayane Cristina <68652767+raydevcp20@users.noreply.github.com>
2021-12-17 00:42:07 -0300

    change settings

--- api/index.js
+++ api/index.js
@@ -15,12 +15,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //get permisions and disable CORS
-app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header, Authorization');
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Custom-Header, Authorization');
+  app.use(cors());
     if (req.method === 'OPTIONS') {
       return res.status(200).end();
     }
@@ -31,7 +30,7 @@ app.use('/auth', authRoutes);
 app.use('/auth', postsRoutes);
 app.use('/auth', photoRoutes);
 
-
-app.listen(3000, () =>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
     console.log('Server Conectado.');
 });
